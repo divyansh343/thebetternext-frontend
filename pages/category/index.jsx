911475ac8cb2c8eff7aa7337711284a54a71453a
divyansh pal <70535666+divyansh343@users.noreply.github.com>
2022-08-05 19:54:08 +0530

    category page fetching working

--- pages/category/index.jsx
+++ pages/category/index.jsx
@@ -1,18 +1,52 @@
+import Image from 'next/image';
 import Link from 'next/link'
 import React from 'react'
 import client from '../../client';
 
-const index = () => {
+import imageUrlBuilder from '@sanity/image-url'
+const builder = imageUrlBuilder(client)
+
+function urlFor(source) {
+  return builder.image(source)
+}
+const index = ({ health_posts }) => {
   return (
     <div>
-      <p className='pt-14 text-4xl'>
-        Fuckin all categories
-        <Link href='category/sports'>
-          <a>
-            sports
-          </a>
-        </Link>
-      </p>
+      <div className="mx-10 heading font-bold text-4xl m-5 text-primary">Lifestyle</div>
+      <div className="holder mx-auto w-10/12 grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
+        {
+          // props.map(allPosts => (<> {
+
+            health_posts.map((post) => (
+            <>
+
+              <div key={post._id} className="each mb-10 m-2 shadow-lg border-gray-800 bg-white relative">
+
+                {post.mainImage &&
+                  <Image
+                    key={post.slug.current}
+                    width={280}
+                    height={300}
+                    className="w-full"
+                    src={builder.image(post.mainImage)
+                      .url() || "https://images.unsplash.com/photo-1619866640467-86547b9858d1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aW5kaWF8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60"}
+                    alt={post.title} />
+                }
+
+                <div className="badge absolute top-0 right-0 bg-primary m-1 text-white p-1 px-2 text-xs font-bold rounded">10:53</div>
+                <div className="desc p-4 text-gray-800">
+                  <Link href={`/category/sports/${post.slug.current}`}>
+                    <a className="title font-bold text-primary block cursor-pointer hover:underline">{post.title}</a>
+                  </Link>
+                </div>
+              </div>
+            </>
+          ))
+          // }
+          // </>
+          // ))
+        }
+      </div>
     </div>
   )
 }
@@ -21,7 +55,7 @@ export async function getServerSideProps() {
 
   // for displaying post categorywise.
 
-  const query = `
+  const lifestyle_query = `
   *[_type == "post" &&  "lifestyle" in categories[]->title]{
     author->{
       name,
@@ -30,8 +64,17 @@ export async function getServerSideProps() {
     },
     ...,
     }`;
-  const data = await client.fetch(query)
-  console.log({"category posts":data});
+  const lifestyle_posts = await client.fetch(lifestyle_query)
+  const health_query = `
+  *[_type == "post" &&  "health" in categories[]->title]{
+    author->{
+      name,
+      slug,
+      image
+    },
+    ...,
+    }`;
+  const health_posts = await client.fetch(health_query)
 
   // for all authors
 
@@ -48,7 +91,12 @@ export async function getServerSideProps() {
   // console.log({ "category posts": data });
 
   return {
-    props: {}, // will be passed to the page component as props
+    props: {
+      lifestyle_posts,
+      health_posts
+      // allposts: [lifestyle_posts,
+      //   health_posts]
+    }, // will be passed to the page component as props
   }
 }
 
