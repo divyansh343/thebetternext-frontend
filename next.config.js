95cb059271e2566b3dcc4489ed64a0e09d8495c1
divyansh343 <divyanshpal402@gmail.com>
2023-02-12 10:59:42 +0530

    perfect pwa

--- next.config.js
+++ next.config.js
@@ -1,16 +1,13 @@
-const withPWA = require("next-pwa");
 /** @type {import('next').NextConfig} */
+
+
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
 
 module.exports = withPWA({
   reactStrictMode: true,
   images: {
     domains: ['images.unsplash.com','www.hyperui.dev','placeimg.com','cdn.sanity.io','source.unsplash.com', 'tuk-cdn.s3.amazonaws.com','i.ibb.co','img.freepik.com'],
   },
-	pwa: {
-    pwa: {
-      dest: "public",
-      register: true,
-      skipWaiting: true,
-    },
-	},
-});
+})
--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,10 @@ class MyDocument extends Document {
                 <Head>
                 <link rel="manifest" href="/manifest.json" />
                 <link rel="apple-touch-icon" href="/icon-192x192.png"></link>
+                <meta
+  name='viewport'
+  content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
+/>
           <meta name="theme-color" content="#fff" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" /><link href="https://fonts.googleapis.com/css2?family=Alata&family=Mingzat&family=Inter&family=Andika&family=Poppins&family=Lato&family=Sniglet&display=swap" rel="stylesheet" />
