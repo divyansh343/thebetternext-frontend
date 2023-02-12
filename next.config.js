6d395fc4afc3b0e1a1058b5bc629f156b875a995
divyansh343 <divyanshpal402@gmail.com>
2023-02-12 10:31:57 +0530

    making pwa

--- next.config.js
+++ next.config.js
@@ -6,3 +6,12 @@ module.exports = {
     domains: ['images.unsplash.com','www.hyperui.dev','placeimg.com','cdn.sanity.io','source.unsplash.com', 'tuk-cdn.s3.amazonaws.com','i.ibb.co','img.freepik.com'],
   },
 }
+const withPWA = require("next-pwa");
+module.exports = withPWA({
+	pwa: {
+		dest: "public",
+		register: true,
+        disable: process.env.NODE_ENV ===      'development',
+		skipWaiting: true,
+	},
+});
--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
+                <link rel="manifest" href="/manifest.json" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" /><link href="https://fonts.googleapis.com/css2?family=Alata&family=Mingzat&family=Inter&family=Andika&family=Poppins&family=Lato&family=Sniglet&display=swap" rel="stylesheet" />
                 <link href="https://fonts.cdnfonts.com/css/sf-pro-display" rel="stylesheet" />
